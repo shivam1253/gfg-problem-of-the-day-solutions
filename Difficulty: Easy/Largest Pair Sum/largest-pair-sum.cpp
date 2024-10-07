b44867cbf4137961b8af0fb7931a918f3c36f082
Shivam Sahu <151868107+shivam1253@users.noreply.github.com>
2024-10-08 00:02:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Largest Pair Sum/largest-pair-sum.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int pairsum(vector<int> &arr) {
+        int p=INT_MIN;
+        for(auto it:arr){
+            p=max(p,it);
+        }
+        int q=INT_MIN;
+        for(auto it:arr){
+            if(it!=p){
+                q=max(q,it);
+            }
+        }
+        return p+q;
+        // code here
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution ob;
+        int r = ob.pairsum(arr);
+        cout << r << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
